b2c305c857032014bb4194abf83b4b617d46255d
Taras Holets <taraskinla@gmail.com>
2020-10-06 21:34:18 +0200

    Setup search

--- WeatherApp/Scenes/CitiesListing/CitiesListingViewController.swift
+++ WeatherApp/Scenes/CitiesListing/CitiesListingViewController.swift
@@ -1,19 +1,31 @@
 import UIKit
+import SnapKit
 
 protocol CitiesListingView: class {
 
 }
 
 class CitiesListingViewController: UIViewController, CitiesListingView {
+    
+    private let searchController = UISearchController(searchResultsController: nil)
 
     var presenter: CitiesListingPresenter?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        presenter?.fetchWeather(for: "London")
-
+        view.backgroundColor = .white
+        setupSearchController()
     }
-
-
+    
+    private func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+    }
 }
 
+extension CitiesListingViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else { return }
+        presenter?.fetchWeather(for: text)
+    }
+}
