d58b6dc0c125d1547a8d03c2c9a9a7b592bbb658
Taras Holets <taraskinla@gmail.com>
2020-10-08 18:17:57 +0200

    Add CodingKeys to resolve snake case

--- WeatherApp/Models/Temperature.swift
+++ WeatherApp/Models/Temperature.swift
@@ -2,6 +2,12 @@ import Foundation
 
 struct Temperature: Codable, Equatable {
     let temp: Double
-    let temp_max: Double
-    let temp_min: Double
+    let tempMax: Double
+    let tempMin: Double
+    
+    private enum CodingKeys: String, CodingKey {
+        case temp
+        case tempMax = "temp_max"
+        case tempMin = "temp_min"
+    }
 }
