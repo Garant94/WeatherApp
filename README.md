a9155fa5027bf1aea9559f7383123d97dadc6347
Taras Holets <taraskinla@gmail.com>
2020-10-09 09:16:40 +0200

    Remove api key, update Readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WeatherApp
 run `pod install`
+
+Please use our own `api key` form https://home.openweathermap.org/api_keys and put it into `ApiConfig.apiKey`
--- WeatherApp/Api/ApiConfig.swift
+++ WeatherApp/Api/ApiConfig.swift
@@ -2,5 +2,5 @@ import Foundation
 
 struct ApiConfig {
     static let baseUrl = "https://api.openweathermap.org/data/2.5/"
-    static let apiKey = "469f68fd7399154c248ad482d32e4cd3"
+    static let apiKey = "" // TODO: - put your client key here
 }
